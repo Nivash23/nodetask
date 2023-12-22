dfaab391e0fc8419294e827380307cf5eff2beed
Nivash23 <nivashsubash32@gmail.com>
2023-12-22 11:48:25 +0530

    node task server

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const PORT = 3004;
+const HOSTNAME = '127.0.0.1';
+
+
+module.exports = { PORT, HOSTNAME };
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const app = require('./App');
+const { PORT, HOSTNAME } = require('./config');
+
+const content = new Date().toISOString();
+const path = new Date();
+
+fs.writeFile('./date-time.txt', content, { flag: 'w+' }, err => {
+    if (err)
+    console.log(err);
+else
+{
+    
+    console.log('content written sucess');
+}
+
+
+})
+app.get('/api/fstext', (req, res) => {
+    
+    fs.readFile('./date-time.txt', 'utf-8', (err, data) => {
+        
+        if (err)
+            console.log(err);
+        else
+            res.status(200);
+            res.send(data);
+    })
+
+})
+
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server listening..`);
+    
+    
+});
+
+
