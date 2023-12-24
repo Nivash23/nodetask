0fbd3318749a38d8e2dc2a32fb3096842711b348
Nivash23 <nivashsubash32@gmail.com>
2023-12-24 15:11:45 +0530

    ...

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-const fs = require('fs');
-const app = require('./App');
-const { PORT, HOSTNAME } = require('./config');
+const fs = require("fs");
+const app = require("./App");
+const { PORT } = require("./config");
 
 const content = new Date().toISOString();
 const path = new Date();
 
-fs.writeFile('./date-time.txt', content, { flag: 'w+' }, err => {
-    if (err)
-    console.log(err);
-else
-{
-    
-    console.log('content written sucess');
-}
-
-
-})
-app.get('/api/fstext', (req, res) => {
-    
-    fs.readFile('./date-time.txt', 'utf-8', (err, data) => {
-        
-        if (err)
-            console.log(err);
-        else
-            res.status(200);
-            res.send(data);
-    })
-
-})
-
-app.listen(PORT, HOSTNAME, () => {
-    console.log(`Server listening..`);
-    
-    
+fs.writeFile("./date-time.txt", content, { flag: "w+" }, (err) => {
+  if (err) console.log(err);
+  else {
+    console.log("content written sucess");
+  }
+});
+app.get("/api/fstext", (req, res) => {
+  fs.readFile("./date-time.txt", "utf-8", (err, data) => {
+    if (err) console.log(err);
+    else res.status(200);
+    res.send(data);
+  });
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server listening..`);
+});
